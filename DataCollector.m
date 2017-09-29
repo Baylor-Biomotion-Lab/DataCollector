b0e9697172b326d442596202d523778f91b408a0
TylerJost <32278047+TylerJost@users.noreply.github.com>
2017-09-29 17:10:02 -0500

    Changed for EMG
    Made it so EMG can be imported and nothing else will be affected.

--- DataCollector.m
+++ DataCollector.m
@@ -1,7 +1,9 @@
-% Main program for collecting and analyzing data from Vicon
-%% Model Output Information
+% Main script for collecting and analyzing data from Vicon
 clearvars -Except FileLocation Errors errorCount ToPull
+%% User Input
 ModifyFootEvents=0; %Set to 1 if you want to put in your own foot events
+saveFile='H:\Research\MATLAB\VAC\CollectedData\Carley';
+%% Model Output Information
 vicon=ViconNexus(); %for more info put doc ViconNexus into command line
 vicon.Connect()
 S = vicon.GetSubjectNames();
@@ -12,7 +14,7 @@ components=cell(1,names); types=cell(1,names);
 for name=1:names
     data{name}=vicon.GetModelOutput(S,Output{name});
     [group{name}, components{name}, types{name}]=...
-    vicon.GetModelOutputDetails(S,Output{name});
+        vicon.GetModelOutputDetails(S,Output{name});
     data{name}=data{name}';
 end
 group=group'; components=components'; types=types';
@@ -20,16 +22,8 @@ ModelOutputHelp=table((1:length(Output))',Output', data', group, components, typ
 ModelOutput=data';
 disp('Model data successfuly imported...')
 %% Force Plate Information
-[ForcePlateData]=ForcePlateInfo(vicon);
+[ForcePlateData, deviceNumbers]=ForcePlateInfo(vicon);
 disp('Force Data successfully imported...')
-%% Trajectory Information
-[trajectories, velocities, accelerations, MarkerHelp]=TrajectoryInfo(vicon, S);
-disp('Trajectories successfully imported...')
-%% Foot Events
-%Run a pipeline which runs "Autocorrelate Events" and "Detect Events From Forceplate" to get all foot marker
-%data
-% vicon.RunPipeline('MyPipeline','Private',45);
-[FootEventStruct, FootEventCell]=GetFootEvents( vicon, S, ModifyFootEvents );
 %% EMG Data
 try
     [ EMGData, EMGNames, EMGTable, EMGProcessed ] = GetEMGData( vicon );
@@ -38,28 +32,37 @@ catch
     EMGTable=NaN;
     EMGProcessed=NaN;
 end
-%% Gait Analysis
-[FootEventCell, RightStancePhase, LeftStancePhase, RightSwingPhase, LeftSwingPhase]=GaitFinder(FootEventCell, trajectories, vicon, S);
-disp('Gaits successfully found...')
-[ModelOutput, ModelOutputHelp, FootEventCell, GaitFail]=MaxMin(ModelOutput,ModelOutputHelp, FootEventCell, RightStancePhase, LeftStancePhase, RightSwingPhase, LeftSwingPhase);
-disp('Data successfully analyzed...')
-if GaitFail~=1
-    [RightStride, LeftStride]=  StrideFinder(RightSwingPhase, LeftSwingPhase, RightStancePhase, LeftStancePhase, trajectories, vicon, S);
+%%
+if ~isnan(ForcePlateData)
+    %% Trajectory Information
+    [trajectories, velocities, accelerations, MarkerHelp]=TrajectoryInfo(vicon, S);
+    disp('Trajectories successfully imported...')
+    %% Foot Events
+    % Run a pipeline which runs "Autocorrelate Events" and "Detect Events From Forceplate" to get all foot marker
+    % data
+    [FootEventStruct, FootEventCell]=GetFootEvents( vicon, S, ModifyFootEvents );
+    
+    %% Gait Analysis
+    [FootEventCell, RightStancePhase, LeftStancePhase, RightSwingPhase, LeftSwingPhase]=GaitFinder(FootEventCell, trajectories, vicon, S);
+    disp('Gaits successfully found...')
+    [ModelOutput, ModelOutputHelp, FootEventCell, GaitFail]=MaxMin(ModelOutput,ModelOutputHelp, FootEventCell, RightStancePhase, LeftStancePhase, RightSwingPhase, LeftSwingPhase);
+    disp('Data successfully analyzed...')
+    if GaitFail~=1
+        [RightStride, LeftStride]=  StrideFinder(RightSwingPhase, LeftSwingPhase, RightStancePhase, LeftStancePhase, trajectories, vicon, S);
+    end
 end
 %% Save Data
 clearvars -Except ModelOutput ModelOutputHelp S trajectories...
     TrajectoryHelp ForcePlateData vicon velocities accelerations...
     FootEventStruct FootEventCell EMGTable RightStancePhase...
     LeftStancePhase RightSwingPhase LeftSwingPhase RightStride LeftStride...
-    FileLocation Errors errorCount ToPull EMGProcessed
+    FileLocation Errors errorCount ToPull EMGProcessed saveFile
 MarkerNames=vicon.GetMarkerNames(S)';
 [ startFrame, endFrame ] = vicon.GetTrialRegionOfInterest;
 
 %Put filepath here for where you want to save data
-cd 'H:\Research\MATLAB\VAC\CollectedData\Carley'
+cd(saveFile)
 
 [~, TrialName]=vicon.GetTrialName;
 fprintf('Finished with trial %s \n', TrialName)
 save(TrialName);
-%% Test Data and Name Concatenation
-%  cd 'H:\Research\MATLAB\DataCollector\CollectedData'
